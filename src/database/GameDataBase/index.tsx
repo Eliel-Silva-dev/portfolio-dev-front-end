48f0398ca1f122b25bc19ccba6ab0208502ba613
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-27 17:19:05 -0300

    feat: react portfolio games data base

---
+++ src/database/GameDataBase/index.tsx
@@ -0,0 +1,28 @@
+const GameDataProject = [
+  {
+    src: '/img/imgcardsnake.jpg',
+    alt: 'Imagem ilustrativa do jogo snake',
+    title: 'Snake js',
+    h3: 'Snake js',
+    p: 'Jogo snake desenvolvido com react e next js',
+    href_site: '/',
+    txt_site: 'Site',
+    href_git: '/',
+    txt_git: 'Repositório',
+    status: false,
+  },
+  {
+    src: '/img/imgcardmario.jpg',
+    alt: 'imagem ilustrativa do jogo mario',
+    title: 'Mario js',
+    h3: 'Mario js',
+    p: 'Jogo mario desenvolvido com react e next js',
+    href_site: '/',
+    txt_site: 'Site',
+    href_git: '/',
+    txt_git: 'Repositório',
+    status: false,
+  },
+];
+
+export default GameDataProject;
