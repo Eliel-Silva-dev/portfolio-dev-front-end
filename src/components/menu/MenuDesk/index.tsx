282f16e7911e9b6ccef43365c13ce21cb88541c3
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-28 13:26:31 -0300

    feat: react portfolio menu link game mob update

--- src/components/menu/MenuDesk/index.tsx
+++ src/components/menu/MenuDesk/index.tsx
@@ -1,26 +1,16 @@
-'use client';
-
-import { useRef } from 'react';
 import style from './style.min.module.css';
 
 import Link from 'next/link';
 
 const MenuDesk = () => {
-  const gameslink = useRef({} as HTMLUListElement);
-
-  const showGamesLink = () => {
-    gameslink.current.classList.toggle('style_min_active__Qnc3_');
-  };
   return (
     <ul className={style.menu_desk}>
       <li>
         <Link href={'/'}>Inicio</Link>
       </li>
       <li className={style.container_LinkGames}>
-        <Link onClick={showGamesLink} href={''}>
-          Projetos
-        </Link>
-        <ul ref={gameslink} className={`${style.game_links} ${style.active}`}>
+        <Link href={''}>Projetos</Link>
+        <ul className={`${style.game_links} ${style.active}`}>
           <li>
             <Link href={'/projects'}>Sites</Link>
           </li>
